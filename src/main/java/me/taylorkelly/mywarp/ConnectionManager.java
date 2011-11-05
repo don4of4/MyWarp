a5bda65500349ec17580d8491d76154bd17baf7d
Spathi <spathi@playminecraft.net>
2011-11-05 12:55:32 +0000

    Fixing idle mysql connection errorspam

--- src/main/java/me/taylorkelly/mywarp/ConnectionManager.java
+++ src/main/java/me/taylorkelly/mywarp/ConnectionManager.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class ConnectionManager {
     private static Connection conn;
         
@@ -29,20 +28,35 @@ public class ConnectionManager {
         return conn;
     }
 
-    public static Connection getConnection() {
-        if(conn == null) conn = initialize();
-      	return conn;
-    }
+	public static Connection getConnection() {
+		if(conn == null) conn = initialize();
+		if(WarpSettings.usemySQL) {
+			// We probably dont need to do this for SQLite. 
+			try {
+				if(!conn.isValid(10)) conn = initialize();
+			} catch (SQLException ex) {
+				WarpLogger.severe("Failed to check SQL status", ex);
+			}
+		}
+		return conn;
+	}
 
     public static void closeConnection() {
-        if(conn != null) {
-            try {
-                conn.close();
-                conn = null;
-            } catch (SQLException ex) {
-                WarpLogger.severe("Error on Connection close", ex);
-            }
-        }
+		if(conn != null) {
+			try {
+				if(WarpSettings.usemySQL){
+					if(conn.isValid(10)) {
+						conn.close();
+					}
+					conn = null;
+				} else {
+					conn.close();
+					conn = null;
+				}
+			} catch (SQLException ex) {
+				WarpLogger.severe("Error on Connection close", ex);
+			}
+		}
     }
 
 
